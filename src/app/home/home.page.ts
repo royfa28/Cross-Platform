1531751087f311c566ae2cd70ab30c62de5803be
johannes <jmuljana@gmail.com>
2019-11-12 00:57:35 +1100

    removed Observable from import in home ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup} from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import {timer} from 'rxjs';
 
 import { DataService } from '../data.service';
